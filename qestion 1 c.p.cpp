92ca0bdbb4889fbb77718cef409402af6f2110a4
pradeeppradeepkumar4269-crypto <pradeeppradeepkumar4269@gmail.com>
2025-09-20 14:38:56 +0530

    Add files via upload

---
+++ qestion 1 c.p.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int n,count=1;
+	scanf("%d\n",&n);
+	char str[n];
+	scanf("[^n]",str);
+	for(int i=0;str[i]!='\0';i++);{
+	if (str[1]==' ');
+		count++;
+    }
+	printf("%d",count);
+   return 0;
+}
